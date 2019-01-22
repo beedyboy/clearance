490c47e015965690d2687df278450ea8633a53dc
beedyboy <boladebode@gmail.com>
2019-01-22 17:00:12 +0100

    account section updated

--- account/admin.py
+++ account/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
 
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 # Register your models here.
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['username', 'email']
+User = get_user_model()
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
 
-admin.site.register(CustomUser, CustomUserAdmin)
+class UserAdmin(BaseUserAdmin):
+
+    form = UserAdminChangeForm # edit view
+    add_form = UserAdminCreationForm #create view
+    inlines = [ProfileInline]
+    # class Meta:
+    #     model =User
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('user_id', 'first_name',  'last_name', 'admin', )
+    list_filter = ('admin', 'staff', 'active')
+    fieldsets = (
+        (None, {'fields': ('user_id', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name',)}),
+        ('Permissions', {'fields': ('admin', 'staff',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('user_id', 'password1', 'password2', 'first_name', 'last_name',)}
+         ),
+    )
+
+    search_fields = ('user_id', 'first_name')
+    ordering = ('user_id',)
+    filter_horizontal = ()
+User = get_user_model()
+
+admin.site.register(User, UserAdmin)
--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import LoginView, RegisterView
 
 app_name = 'account'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', views.login_view, name ="login"),
     path('login', views.login_view, name ="login"),
     path('logout', views.logout_view, name ="logout"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('register/', RegisterView.as_view(), name="register"),
 ]
